721faa4c375524736d00a5997d9c33d06f38ed9c
Jon Mifsud <info@jonmifsud.com>
2015-03-19 13:36:06 +0100

    make dropzone generate files of the right format as pngs take more space

--- assets/dropzone.js
+++ assets/dropzone.js
@@ -1234,7 +1234,7 @@ require.register("dropzone/lib/dropzone.js", function (exports, module) {
             canvas.width = resizeInfo.trgWidth;
             canvas.height = resizeInfo.trgHeight;
             drawImageIOSFix(ctx, img, (_ref = resizeInfo.srcX) != null ? _ref : 0, (_ref1 = resizeInfo.srcY) != null ? _ref1 : 0, resizeInfo.srcWidth, resizeInfo.srcHeight, (_ref2 = resizeInfo.trgX) != null ? _ref2 : 0, (_ref3 = resizeInfo.trgY) != null ? _ref3 : 0, resizeInfo.trgWidth, resizeInfo.trgHeight);
-            thumbnail = canvas.toDataURL("image/png");
+            thumbnail = canvas.toDataURL(file.type);
             _this.emit("thumbnail", file, thumbnail);
             if (callback != null) {
               return callback();
